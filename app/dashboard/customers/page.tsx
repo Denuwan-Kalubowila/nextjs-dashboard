0830c5efa7fed6f6f8839c08925cb49cd396b437
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2023-11-28 22:35:50 +0530

    next14 basics

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+const Customers =()=>{
+    return(
+        <>
+            <h2>Customers
+             Page</h2>
+        </>
+    )
+}
+
+export default Customers
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+const Invoices =()=>{
+    return(
+        <>
+            <h2>Invoices Page</h2>
+        </>
+    )
+}
+
+export default Invoices
